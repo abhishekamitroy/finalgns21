3ac017f490c772d77ee0905d1ad05c3142ae27a2
abhishekroy <abhishekamitroy@gmail.com>
2024-10-06 10:34:32 -0700

    2

--- aro.py
+++ aro.py
@@ -70,12 +70,12 @@ if uploaded_file is not None:
         template='plotly_dark',
         xaxis_tickangle=-45,
         hovermode='x unified',
-        font=dict(family='Roboto, sans-serif', color='#ffffff', size=24),
+        font=dict(family='Roboto, sans-serif', color='#ffffff', size=20),
         plot_bgcolor='#1f2c56',
         paper_bgcolor='#1f2c56',
-        title_font=dict(size=32),
-        legend_font=dict(size=26),
-        coloraxis_colorbar=dict(title_font=dict(size=28), tickfont=dict(size=24))
+        title_font=dict(size=38),
+        legend_font=dict(size=20),
+        coloraxis_colorbar=dict(title_font=dict(size=24), tickfont=dict(size=20))
     )
 
     st.plotly_chart(fig_vulnerability, use_container_width=True)
